e8ad30ecd48f7212567a35216a7fd57102619ef2
Alberto Federico Turelli <alberto.turelli@gmail.com>
2025-04-10 22:23:52 +0200

    Importazione budget 2025 - TEST

---
+++ Tables/dbo.Budget2025$.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [dbo].[Budget2025$]
+(
+[Agente] [nvarchar] (255) COLLATE Latin1_General_CI_AS NULL,
+[Mese] [nvarchar] (255) COLLATE Latin1_General_CI_AS NULL,
+[Importo] [decimal] (28, 12) NULL
+) ON [PRIMARY]
+GO
