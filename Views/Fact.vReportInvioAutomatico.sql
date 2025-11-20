5cc7b787d3b5374e99c73d604813900122ecdb0d
Alberto Federico Turelli <alberto.turelli@gmail.com>
2025-11-20 19:47:00 +0100

    Report Accessi e Accessi Demo: +Paolo Dosi

--- Views/Fact.vReportInvioAutomatico.sql
+++ Views/Fact.vReportInvioAutomatico.sql
@@ -2,6 +2,7 @@ SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
 GO
+
 /**
  * @view Fact.vReportInvioAutomatico
 */
@@ -24,7 +25,7 @@ AS (
     SELECT
         N'Accessi',
         --N'cipriani@cesimultimedia.it;paola.turolla@cesimultimedia.it;giuseppe.lobrano@cesimultimedia.com;valeria.barbaglia@cesimultimedia.it;antonio.loprevite@cesimultimedia.it;andrea.giuggioli@cesimultimedia.it;eleonora.soravia@cesimultimedia.it;valentina.borroni@cesimultimedia.it',
-        N'gabriella.mottica@cesimultimedia.it;cipriani@cesimultimedia.it;paola.turolla@cesimultimedia.it;andrea.giuggioli@cesimultimedia.it;angela.battaglia@cesimultimedia.it;giada.lidonnici@cesimultimedia.it;serena.leso@cesimultimedia.it;nicola.mastropietro@cesimultimedia.it;anna.defeudis@cesimultimedia.it',
+        N'gabriella.mottica@cesimultimedia.it;cipriani@cesimultimedia.it;paola.turolla@cesimultimedia.it;andrea.giuggioli@cesimultimedia.it;angela.battaglia@cesimultimedia.it;giada.lidonnici@cesimultimedia.it;serena.leso@cesimultimedia.it;nicola.mastropietro@cesimultimedia.it;anna.defeudis@cesimultimedia.it;paolo.dosi@cesimultimedia.it',
         N'Report Accessi',
         NULL
 
@@ -99,7 +100,7 @@ AS (
     SELECT
         N'Accessi Demo',
         --N'cipriani@cesimultimedia.it;paola.turolla@cesimultimedia.it;giuseppe.lobrano@cesimultimedia.com;valeria.barbaglia@cesimultimedia.it;antonio.loprevite@cesimultimedia.it;andrea.giuggioli@cesimultimedia.it;eleonora.soravia@cesimultimedia.it;valentina.borroni@cesimultimedia.it',
-        N'gabriella.mottica@cesimultimedia.it;cipriani@cesimultimedia.it;paola.turolla@cesimultimedia.it;andrea.giuggioli@cesimultimedia.it;angela.battaglia@cesimultimedia.it;giada.lidonnici@cesimultimedia.it;serena.leso@cesimultimedia.it;nicola.mastropietro@cesimultimedia.it;anna.defeudis@cesimultimedia.it',
+        N'gabriella.mottica@cesimultimedia.it;cipriani@cesimultimedia.it;paola.turolla@cesimultimedia.it;andrea.giuggioli@cesimultimedia.it;angela.battaglia@cesimultimedia.it;giada.lidonnici@cesimultimedia.it;serena.leso@cesimultimedia.it;nicola.mastropietro@cesimultimedia.it;anna.defeudis@cesimultimedia.it;paolo.dosi@cesimultimedia.it',
         N'Report Accessi Demo',
         NULL
 )
