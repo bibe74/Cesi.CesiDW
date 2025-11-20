bdd77c555d8f0ed294426e5e318621f38b5d499d
Alberto Federico Turelli <alberto.turelli@gmail.com>
2025-11-20 19:50:38 +0100

    Import.Amministratori: -Eleonora Soravia

--- Data/Import.Amministratori_Data.sql
+++ Data/Import.Amministratori_Data.sql
@@ -1,7 +1,6 @@
 INSERT INTO [Import].[Amministratori] ([Amministratore], [ADUser], [Email]) VALUES (N'Cipriani Elio', N'CESI\Elio Cipriani', N'elio.cipriani@cesimultimedia.com')
 INSERT INTO [Import].[Amministratori] ([Amministratore], [ADUser], [Email]) VALUES (N'Cipriani Elio (VPN)', N'CESI\CiprianiVPN', N'')
 INSERT INTO [Import].[Amministratori] ([Amministratore], [ADUser], [Email]) VALUES (N'Cipriani Valentina', N'CESI\Valentina Cipriani', N'valentina.cipriani@cesimultimedia.it')
-INSERT INTO [Import].[Amministratori] ([Amministratore], [ADUser], [Email]) VALUES (N'Eleonora Soravia', N'CESI\Eleonora Soravia', N'eleonora.soravia@cesimultimedia.com')
 INSERT INTO [Import].[Amministratori] ([Amministratore], [ADUser], [Email]) VALUES (N'Giuggioli Andrea', N'CESI\Andrea Giuggioli', N'andrea.giuggioli@cesimultimedia.com')
 INSERT INTO [Import].[Amministratori] ([Amministratore], [ADUser], [Email]) VALUES (N'Mastropietro Nicola', N'CESI\Nicola Mastropietro', N'nicola.mastropietro@cesimultimedia.it')
 INSERT INTO [Import].[Amministratori] ([Amministratore], [ADUser], [Email]) VALUES (N'Mottica Gabriella', N'CESI\Gabriella Mottica', N'gabriella.mottica@cesimultimedia.it')
